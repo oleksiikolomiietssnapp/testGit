7f6649fe5f3ac353919ce75eec969f9ead9f90ce
Stepan19897 <stepan.niemykin@gmail.com>
2020-09-26 18:18:27 +0300

    created Stepan.storyboard and safariView for google.com

---
+++ TestGitHubProj/StepanViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StepanViewController.swift
+//  TestGitHubProj
+//
+//  Created by Stepan Niemykin on 26.09.2020.
+//
+
+import UIKit
+import SafariServices
+
+class StepanViewController: UIViewController {
+
+
+    @IBAction func googleButton(_ sender: UIButton) {
+        showSafari("https://www.google.com/?client=safari")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func showSafari(_ url: String) {
+        guard let url = URL(string: url) else { return }
+        
+        let safariViewController = SFSafariViewController(url: url)
+        present(safariViewController, animated: true, completion: nil)
+    }
+
+}

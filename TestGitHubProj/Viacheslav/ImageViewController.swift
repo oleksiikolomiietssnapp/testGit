fa020b5100d9d350f819cdbff708e9b1e22c12e8
Viacheslav <markvyach83@gmail.com>
2020-09-29 22:06:36 +0300

    initial commit, create ImageStoryboard and ImageViewController

---
+++ TestGitHubProj/Viacheslav/ImageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ImageViewController.swift
+//  TestGitHubProj
+//
+//  Created by Viacheslav Markov on 29.09.2020.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

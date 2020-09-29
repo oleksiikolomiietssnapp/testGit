1ede9a039076e987d4507121ef558cf909163ace
Viacheslav <markvyach83@gmail.com>
2020-09-29 22:30:32 +0300

    added scrollView, closedButton and imageView on ImageViewController

--- TestGitHubProj/Viacheslav/ImageViewController.swift
+++ TestGitHubProj/Viacheslav/ImageViewController.swift
@@ -8,22 +8,21 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var closedButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        closedButton.isHidden = true
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchClosedButton(_ sender: UIButton) {
     }
-    */
-
+    
 }

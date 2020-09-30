097834600f924803725a198670f2d4365b03ff5c
Viacheslav <markvyach83@gmail.com>
2020-09-30 14:01:37 +0300

    added foot avatar.jpg and write some code

--- TestGitHubProj/Viacheslav/ImageViewController.swift
+++ TestGitHubProj/Viacheslav/ImageViewController.swift
@@ -9,20 +9,42 @@ import UIKit
 
 class ImageViewController: UIViewController {
     
-    @IBOutlet var scrollView: UIScrollView!
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var closedButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var closedButton: UIButton!
     
-
+    
+    let maxSize: CGFloat = 2
+    let minSize: CGFloat = 0.5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         closedButton.isHidden = true
-
-
+        
+        scrollView.maximumZoomScale = maxSize
+        scrollView.minimumZoomScale = minSize
+        
+        scrollView.delegate = self
     }
     
     @IBAction func touchClosedButton(_ sender: UIButton) {
+        
+        closedButton.isHidden = true
+        scrollView.zoomScale = minSize * 2
     }
     
+}
+
+extension ImageViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale >= maxSize {
+            closedButton.isHidden = false
+        }
+    }
 }

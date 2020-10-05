8832dadbaa6e11cab22ece69fb80135567642d79
svitlana.korostelova <svitlana.korostelova@gmail.com>
2020-10-05 13:31:23 +0300

    Firebase service basic functions added, new pods added into Podfile

---
+++ TestGitHubProj/Services/FirebaseService.swift
@@ -0,0 +1,99 @@
+//
+//  FirebaseService.swift
+//  TestGitHubProj
+//
+//  Created by Svitlana Korostelova on 04.10.2020.
+//
+
+import Foundation
+import Firebase
+import FirebaseCore
+import FirebaseFirestore
+
+final class FirebaseService {
+    private static let db = Firestore.firestore()
+    
+    /// Add a new document with own ID
+    class func addDataToDB(collectionName: String, documentName: String, dictionaryData: [String:Any]) {
+        db.collection(collectionName).document(documentName).setData(dictionaryData)
+        db.collection(collectionName).document(documentName).updateData([
+            "lastUpdated": FieldValue.serverTimestamp(), //by whom (username)
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document \(documentName) successfully added")
+            }
+        }
+    }
+    
+    /// Add a new document with a generated ID-document
+    class func addDataToDB(collectionName: String, dictionaryData: [String:Any]) -> String? {
+        var ref: DocumentReference?
+        
+        ref = db.collection(collectionName).addDocument(data: dictionaryData) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            }
+        }
+        if let unwrapRef = ref {
+            print("Document added with ID: \(unwrapRef.documentID)")
+            return unwrapRef.documentID
+        }
+        return nil
+    }
+    
+    /// Update one field, creating the document if it does not exist, creating the field if it does not exist
+    class func updateDataToDB(collectionName: String, documentName: String, fieldName: String, value: Any) {
+        db.collection(collectionName).document(documentName).setData([ fieldName: value ], merge: true)
+        db.collection(collectionName).document(documentName).updateData([
+            "lastUpdated": FieldValue.serverTimestamp(), //by whom (username)
+        ]) { err in
+            if let err = err {
+                print("Error updating document: \(err)")
+            } else {
+                print("Document \(documentName) successfully updated")
+            }
+        }
+    }
+    /// Get all documents in a collection
+    class func readDataFromDB(collectionName: String) {
+        db.collection(collectionName).getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
+    }
+    
+    /// Retrieve the contents of a single document
+    class func readDataFromDB(collectionName: String, documentName: String) {
+        let docRef = db.collection(collectionName).document(documentName)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document does not exist")
+            }
+        }
+    }
+    
+    /// Get multiple documents from a collection by selected value
+    class func selectDataFromDBbyValue (collectionName: String, fieldName: String, value: Any) {
+        db.collection(collectionName).whereField(fieldName, isEqualTo: value)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    for document in querySnapshot!.documents {
+                        print("\(document.documentID) => \(document.data())")
+                    }
+                }
+            }
+    }
+}

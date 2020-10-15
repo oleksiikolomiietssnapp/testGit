f62441fa8e186a51491264cc7d0f6cde48bd7890
spanastasia <anastasibohdan45@gmail.com>
2020-10-15 14:40:00 +0300

    remove user image

--- TestGitHubProj/Anastasia/Firebase/ImageCell.swift
+++ TestGitHubProj/Anastasia/Firebase/ImageCell.swift
@@ -7,6 +7,39 @@
 
 import UIKit
 
-class ImageCell: UITableViewCell {
+protocol ImageCellDelegate: AnyObject {
+    func imageCell()
+}
 
+class ImageCell: UITableViewCell {
+    
+    weak var delegate: ImageCellDelegate?
+    
+    @IBOutlet weak var imagePlaceholder: UIImageView!
+    
+    @IBOutlet weak var bucketButton: UIButton!
+    
+    @IBAction func bucketButtonPressed() {
+        delegate?.imageCell()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imagePlaceholder.image = UIImage(named: "userPhotoTemplate")
+        bucketButton.isEnabled = false
+        print("reload data")
+    }
+    
+    func configure(with user: User) {
+        DispatchQueue.main.async {
+            FirebaseService.downloadUserImageFromDB(id: user.documentID) { [weak self] (image) in
+                self?.imagePlaceholder.image = image
+                self?.bucketButton.isEnabled = true
+            }
+        }
+        if imagePlaceholder.image == nil {
+            bucketButton.isEnabled = false
+            imagePlaceholder.image = UIImage(named: "userPhotoTemplate")
+        }
+    }
 }

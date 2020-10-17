794d7e817f1b88bf5a1c47b9d1a709df662bed63
Oleksii Kolomiiets <oleksii.kolomiiets@ext.elli.eco>
2020-10-17 12:43:40 +0300

    attempt to add four animators

--- TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
+++ TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class AnimationFrameViewController: UIViewController {
     lazy var square = { return UIView() }()
-    var animator: UIViewPropertyAnimator!
+    var animator1: UIViewPropertyAnimator!
+    var animator2: UIViewPropertyAnimator!
+    var animator3: UIViewPropertyAnimator!
+    var animator4: UIViewPropertyAnimator!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +24,50 @@ class AnimationFrameViewController: UIViewController {
         super.viewDidAppear(animated)
         square.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         square.translatesAutoresizingMaskIntoConstraints = false
-        animator = UIViewPropertyAnimator(duration: 3, curve: .linear) {
-            self.square.transform = CGAffineTransform.identity.translatedBy(x: self.view.frame.width - 80, y: self.view.frame.height - 80)
+        
+        animator1 = UIViewPropertyAnimator(duration: 3, curve: .linear) {
+            self.square.transform = CGAffineTransform
+                .identity
+                .translatedBy(x: self.view.frame.width - 80, y: 0)
             print(self.view.frame.height)
             print(self.view.frame.maxY)
         }
-        animator.startAnimation()
+        animator1.addCompletion({ _ in
+            self.animator2.startAnimation()
+            
+        })
+        
+        animator2 = UIViewPropertyAnimator(duration: 3, curve: .linear) {
+            self.square.transform = CGAffineTransform
+                .identity
+                .translatedBy(x: self.view.frame.width - 80, y: 200)
+        }
+        animator2.addCompletion { (_) in
+            self.animator3.startAnimation()
+        }
+        
+        animator3 = UIViewPropertyAnimator(duration: 3, curve: .linear) {
+            self.square.transform = CGAffineTransform
+                .identity
+                .translatedBy(x: 0, y: 200)
+        }
+        animator3.addCompletion { (_) in
+            self.animator4.startAnimation()
+        }
+        
+        animator4 = UIViewPropertyAnimator(duration: 3, curve: .linear) {
+            self.square.transform = CGAffineTransform
+                .identity
+                .translatedBy(x: 0, y: 0)
+        }
+        
+        animator1.startAnimation()
+//        1 square.transform = CGAffineTransform
+//            .identity
+//            .translatedBy(x: self.view.frame.width - 80, y: 0)
+//        2 square.transform.translatedBy(x: 0, y: 200)
+//        3 square.transform.translatedBy(x: -(self.view.frame.width - 80), y: 0)
+//        4 square.transform.translatedBy(x: 0, y: -200)
     }
     override func viewWillLayoutSubviews() {
         view.updateConstraints()

56f403cb44c977a04be82fe82e442e321e51a261
spanastasia <anastasibohdan45@gmail.com>
2020-10-17 12:13:56 +0300

    animated element in viewDidAppear()

--- TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
+++ TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
@@ -17,12 +17,14 @@ class AnimationFrameViewController: UIViewController {
         view.backgroundColor = .systemGray
         square.backgroundColor = .red
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         square.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         square.translatesAutoresizingMaskIntoConstraints = false
-        animator = UIViewPropertyAnimator(duration: 3, curve: .easeOut) {
-            self.square.frame = self.square.frame.offsetBy(dx: self.view.bounds.maxX - 80, dy: 0)
+        animator = UIViewPropertyAnimator(duration: 3, curve: .linear) {
+            self.square.transform = CGAffineTransform.identity.translatedBy(x: self.view.frame.width - 80, y: self.view.frame.height - 80)
+            print(self.view.frame.height)
+            print(self.view.frame.maxY)
         }
         animator.startAnimation()
     }

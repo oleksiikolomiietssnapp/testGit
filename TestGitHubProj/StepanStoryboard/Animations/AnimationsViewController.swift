ab3f6eacb51fd9b782ca474e628e4f81be9491e3
Stepan19897 <stepan.niemykin@gmail.com>
2020-10-19 18:12:02 +0300

    animation with property animator (not finished)

--- TestGitHubProj/StepanStoryboard/Animations/AnimationsViewController.swift
+++ TestGitHubProj/StepanStoryboard/Animations/AnimationsViewController.swift
@@ -8,17 +8,25 @@
 import UIKit
 
 class AnimationsViewController: UIViewController {
+    var delay = 0.0
+    var duration = 1.5
 
     @IBOutlet weak var dotsView: UIView!
     @IBAction func animationSwitchPressed(_ sender: UISwitch) {
-        
     }
     
     @IBAction func speedSliderMoved(_ sender: UISlider) {
+        duration = Double(sender.value + 1.0)
+    }
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        delay = 0.0
+        start()
     }
     
+    var animator: UIViewPropertyAnimator?
+    
     //creating 3 labels
-    lazy var generatedLabel: [UILabel] = {
+    lazy var generatedLabels: [UILabel] = {
         var labels = [UILabel]()
         let labelWidth = CGFloat(30)
         let labelHeight = CGFloat(30)
@@ -44,15 +52,24 @@ class AnimationsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for label in generatedLabel {
+        for label in generatedLabels {
             self.dotsView.addSubview(label)
         }
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        animateLabels(labels: generatedLabel)
-        //letsAnimate(labels: generatedLabel)
+        //animateLabels(labels: generatedLabel)
+        start()
+    }
+    
+    func start() {
+        for label in generatedLabels {
+            createAnimators(label: label)
+            animator?.startAnimation(afterDelay: delay)
+            delay += duration / Double(generatedLabels.count)
+        }
     }
     
     func animateLabels(labels: [UILabel]) {
@@ -71,26 +88,22 @@ class AnimationsViewController: UIViewController {
         }
     }
     
-    func letsAnimate(labels: [UILabel], _ reversed: Bool = true) {
-        let duration = 1.0
-        var delay = 0.0
-        
-        for index in 0..<labels.count {
-            let animatorUp = UIViewPropertyAnimator(duration: duration, curve: .easeInOut, animations: {
-                labels[index].frame.origin.y -= 40.0
-                labels[index].layer.opacity = 0.2
-            })
+    func createAnimators(label: UILabel) {
+        let reversedAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn)
+        animator = UIViewPropertyAnimator(duration: duration, curve: .easeIn)
             
-            let animatorDown = UIViewPropertyAnimator(duration: duration, curve: .easeInOut, animations: {
-                labels[index].frame.origin.y += 40.0
-                labels[index].layer.opacity = 1.0
-            })
-            
-            animatorUp.addCompletion {_ in
-                animatorDown.startAnimation(afterDelay: delay)
-            }
-            animatorUp.startAnimation(afterDelay: delay)
-            delay += duration / 3
+        animator?.addAnimations {
+            label.frame.origin.y -= 40.0
+            label.layer.opacity = 0.2
         }
+        
+        reversedAnimator.addAnimations {
+            label.frame.origin.y += 40.0
+            label.layer.opacity = 1.0
+        }
+        
+        animator?.addCompletion({_ in
+            reversedAnimator.startAnimation()
+        })
     }
 }

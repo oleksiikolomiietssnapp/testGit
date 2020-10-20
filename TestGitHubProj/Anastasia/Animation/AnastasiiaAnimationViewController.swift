bd15178f5bf7579df9f71a1d6dd050e24ec4647f
spanastasia <anastasibohdan45@gmail.com>
2020-10-20 18:03:47 +0300

    try to animate using two property animators

--- TestGitHubProj/Anastasia/Animation/AnastasiiaAnimationViewController.swift
+++ TestGitHubProj/Anastasia/Animation/AnastasiiaAnimationViewController.swift
@@ -7,7 +7,30 @@
 
 import UIKit
 
+protocol AnimationDelegate: AnyObject {
+    func startAnimation()
+    func pauseAnimation()
+    func stopAnimation()
+    func continueAnimation()
+}
+
 class AnastasiiaAnimationViewController: UIViewController {
+    static weak var delegate: AnimationDelegate?
+    
+    @IBOutlet weak var repaetSwitch: UISwitch!
+    
+    @IBOutlet weak var reverseSwitch: UISwitch!
+    
+    @IBAction func pauseBtnPressed() {
+        AnastasiiaAnimationViewController.delegate?.pauseAnimation()
+    }
+    @IBAction func playBtnPressed() {
+        AnastasiiaAnimationViewController.delegate?.startAnimation()
+    }
+    @IBAction func stopBtnPressed() {
+        AnastasiiaAnimationViewController.delegate?.stopAnimation()
+    }
+    
     override func viewDidLoad() {
         
     }
--- TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
+++ TestGitHubProj/Anastasia/Animation/AnimationFrameViewController.swift
@@ -8,19 +8,42 @@
 import UIKit
 
 class AnimationFrameViewController: UIViewController {
-    var animationDuration = 10.0
+    var animationDuration = 1.0
+    var reversed = false
+    var repeated = false
+    
     private let squareSideValue = 80.0
+    
+    private var xAxisMovementAnimator: UIViewPropertyAnimator {
+        return UIViewPropertyAnimator(duration: 0, curve: .linear) { [self] in
+            square.center.x = CGFloat(squareMaxXPosition)
+        }
+    }
+    private var yAxisMovementAnimator: UIViewPropertyAnimator {
+        return UIViewPropertyAnimator(duration: animationDuration, curve: .linear) { [self] in
+            square.center.x = CGFloat(squareMaxXPosition)
+        }
+    }
+    
+    private var squarePosition: CGPoint {
+        return square.center
+    }
+    private var viewPerimetr: Double {
+        return Double(view.frame.width + view.frame.height) * 2
+    }
     private var squareMaxXPosition: Double {
-        return Double(view.frame.width) - self.squareSideValue
+        return Double(view.frame.width) - self.squareSideValue / 2
     }
     private var squareMaxYPosition: Double {
-        return Double(view.frame.height) - self.squareSideValue
+        return Double(view.frame.height) - self.squareSideValue / 2
     }
     private var xAxisMovementDuration: Double {
-        return (Double(animationDuration / 2) / (Double(view.frame.height + view.frame.width) - squareSideValue * 2)) * Double(view.frame.width - squareSideValue)
+        let shift = Double(view.frame.width) - squareSideValue
+        return (animationDuration / (viewPerimetr - squareSideValue * 4)) * shift
     }
     private var yAxisMovementDuration: Double {
-        return (Double(animationDuration / 2) / (Double(view.frame.height + view.frame.width)  - squareSideValue * 2)) * Double(view.frame.height - squareSideValue)
+        let shift = Double(view.frame.height) - squareSideValue
+        return (animationDuration / (viewPerimetr - squareSideValue * 4)) * shift
     }
     private var relativeXMovementDuration: Double {
         return xAxisMovementDuration / animationDuration
@@ -33,30 +56,54 @@ class AnimationFrameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        AnastasiiaAnimationViewController.delegate = self
+        view.backgroundColor = .systemBackground
+        view.layer.borderWidth = 0.4
         view.addSubview(square)
-        view.backgroundColor = .systemGray
+        square.frame.size = CGSize(width: squareSideValue, height: squareSideValue)
         square.backgroundColor = .red
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        square.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        square.translatesAutoresizingMaskIntoConstraints = false
-        UIView.animateKeyframes(withDuration: animationDuration,
-                                delay: 0,
-                                options: [.repeat, .calculationModeLinear],
-                                animations: { [self] in
-                                    UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: relativeXMovementDuration) { [self] in
-                                        square.frame.origin.x = view.frame.width - CGFloat(squareSideValue)
-                                    }
-                                    UIView.addKeyframe(withRelativeStartTime: relativeXMovementDuration, relativeDuration: relativeYMovementDuration) { [self] in
-                                        square.frame.origin.y = view.frame.height - CGFloat(squareSideValue)
-                                    }
-                                    UIView.addKeyframe(withRelativeStartTime: relativeYMovementDuration + relativeXMovementDuration, relativeDuration: relativeXMovementDuration) { [self] in
-                                        square.frame.origin.x = 0
-                                    }
-                                    UIView.addKeyframe(withRelativeStartTime: relativeXMovementDuration * 2 + relativeYMovementDuration, relativeDuration: relativeYMovementDuration) { [self] in
-                                        square.frame.origin.y = 0
-                                    }
-                                })
+}
+
+extension AnimationFrameViewController: AnimationDelegate {
+    func startAnimation() {
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 1, delay: 0, options: .curveLinear, animations: xAxisMovementAnimator.startAnimation, completion: { [self]_ in yAxisMovementAnimator.startAnimation()})
+        
+//                UIView.animateKeyframes(withDuration: animationDuration,
+//                                        delay: 0,
+//                                        options: [.repeat, .calculationModeLinear],
+//                                        animations: { [self] in
+//                                            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: relativeXMovementDuration) { [self] in
+//                                                square.center.x = view.frame.width - CGFloat(squareSideValue / 2)
+//                                            }
+//                                            UIView.addKeyframe(withRelativeStartTime: relativeXMovementDuration, relativeDuration: relativeYMovementDuration) { [self] in
+//                                                square.center.y = view.frame.height - CGFloat(squareSideValue / 2)
+//                                            }
+//                                            UIView.addKeyframe(withRelativeStartTime: relativeYMovementDuration + relativeXMovementDuration, relativeDuration: relativeXMovementDuration) { [self] in
+//                                                square.center.x = CGFloat(squareSideValue / 2)
+//                                            }
+//                                            UIView.addKeyframe(withRelativeStartTime: relativeXMovementDuration * 2 + relativeYMovementDuration, relativeDuration: relativeYMovementDuration) { [self] in
+//                                                square.center.y = CGFloat(squareSideValue / 2)
+//                                            }
+//                                        })
+        
     }
+    
+    func pauseAnimation() {
+        UIView.animateKeyframes(withDuration: 0, delay: 0, options: [], animations: { [self] in
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: relativeXMovementDuration) { [self] in
+                square.center = squarePosition
+            }
+        })
+        square.center = square.center
+    }
+    
+    func stopAnimation() {
+        square.center = CGPoint(x: squareSideValue / 2, y: squareSideValue / 2)
+    }
+    
+    func continueAnimation() {
+        
+    }
+    
 }

8d78ff5e6c8b9e0edc3b3e2195076d20f8700c18
spanastasia <anastasibohdan45@gmail.com>
2020-10-27 09:21:08 +0200

    added four animators and action on stop button

--- TestGitHubProj/Anastasia/Animation/AnastasiiaAnimationViewController.swift
+++ TestGitHubProj/Anastasia/Animation/AnastasiiaAnimationViewController.swift
@@ -23,29 +23,13 @@ class AnastasiiaAnimationViewController: UIViewController {
     
     let animationDuration = 4.0
     
-    private var xAxisMovementAnimator: UIViewPropertyAnimator {
-        return UIViewPropertyAnimator(duration: xAxisMovementDuration, curve: .linear) { [self] in
-            animationFrame.animatedElement.center.x = CGFloat(squareMaxXPosition)
-        }
-    }
-    private var yAxisMovementAnimator: UIViewPropertyAnimator {
-        return UIViewPropertyAnimator(duration: yAxisMovementDuration, curve: .linear) { [self] in
-            animationFrame.animatedElement.center.y = CGFloat(squareMaxYPosition)
-        }
-    }
-    
     private var squarePosition: CGPoint {
         return animationFrame.animatedElement.center
     }
     private var viewPerimetr: Double {
         return Double(animationFrame.frame.width + animationFrame.frame.height) * 2
     }
-    private var squareMaxXPosition: CGFloat {
-        return animationFrame.frame.width - animationFrame.animatedElement.frame.width / 2
-    }
-    private var squareMaxYPosition: CGFloat {
-        return animationFrame.frame.height - animationFrame.animatedElement.frame.width / 2
-    }
+    
     private var xAxisMovementDuration: Double {
         let shift = Double(animationFrame.frame.width - animationFrame.animatedElement.frame.width)
         return (animationDuration * shift) / (viewPerimetr - Double(animationFrame.animatedElement.frame.width) * 4)
@@ -55,6 +39,19 @@ class AnastasiiaAnimationViewController: UIViewController {
         return (animationDuration * shift) / (viewPerimetr - Double(animationFrame.animatedElement.frame.width) * 4)
     }
     
+    lazy var animator1 = { return UIViewPropertyAnimator(duration: xAxisMovementDuration, curve: .linear) {
+        self.animationFrame.animatedElement.center.x = self.animationFrame.frame.width - self.animationFrame.animatedElement.frame.width / 2
+    }}()
+    lazy var animator2 = { return UIViewPropertyAnimator(duration: xAxisMovementDuration, curve: .linear) {
+        self.animationFrame.animatedElement.center.y = self.animationFrame.frame.height - self.animationFrame.animatedElement.frame.width / 2
+    }}()
+    lazy var animator3 = { return UIViewPropertyAnimator(duration: xAxisMovementDuration, curve: .linear) {
+        self.animationFrame.animatedElement.center.x = self.animationFrame.animatedElement.frame.width / 2
+    }}()
+    lazy var animator4 = { return UIViewPropertyAnimator(duration: xAxisMovementDuration, curve: .linear) {
+        self.animationFrame.animatedElement.center.y = self.animationFrame.animatedElement.frame.width / 2
+    }}()
+    
     @IBAction func pauseBtnPressed() {
         
     }
@@ -62,37 +59,38 @@ class AnastasiiaAnimationViewController: UIViewController {
         startAnimation()
     }
     @IBAction func stopBtnPressed() {
-        
+        stopAnimation()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animationFrame.delegate = self
+        animator1.addCompletion {_ in
+            self.animator2.startAnimation()
+        }
+        animator2.addCompletion {_ in
+            self.animator3.startAnimation()
+        }
+        animator3.addCompletion {_ in
+            self.animator4.startAnimation()
+        }
     }
 }
 
 extension AnastasiiaAnimationViewController: AnimationDelegate {
     func startAnimation() {
-        xAxisMovementAnimator.isReversed = false
-        xAxisMovementAnimator.startAnimation()
-        xAxisMovementAnimator.addCompletion { _ in
-            self.yAxisMovementAnimator.isReversed = false
-            self.yAxisMovementAnimator.startAnimation()
-        }
-        yAxisMovementAnimator.addCompletion { [self]_ in
-            xAxisMovementAnimator.isReversed = true
-            xAxisMovementAnimator.startAnimation()
-        }
-        xAxisMovementAnimator.addCompletion { [self]_ in
-            yAxisMovementAnimator.isReversed = true
-            yAxisMovementAnimator.startAnimation()
-        }
+        animator1.startAnimation()
     }
 
     func pauseAnimation() {
     }
 
     func stopAnimation() {
+        animator1.stopAnimation(false)
+        animator2.stopAnimation(false)
+        animator3.stopAnimation(false)
+        animator4.stopAnimation(false)
+        animationFrame.animatedElement.frame.origin = .zero
     }
 
     func continueAnimation() {
